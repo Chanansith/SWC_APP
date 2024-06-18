53f64f732a4852da57f611b16f7673de7c7a810d
master <airbitprodev@gmail.com>
2024-06-18 14:56:08 +0700

    add new sess

--- application/config/config.php
+++ application/config/config.php
@@ -384,7 +384,9 @@ $config['encryption_key'] = 'Go87NSzqqq96Dq2jcjnvhVUCpKPeP2ea';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+//$config['sess_save_path'] = NULL;
+ // Custom session save path
+$config['sess_save_path'] = APPPATH . 'sessions/'; 
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
