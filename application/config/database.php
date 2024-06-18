5a7ad7d7c0c4c3840ac6329dfe22aad16ffac86a
master <airbitprodev@gmail.com>
2024-06-18 14:06:18 +0700

    add new config

--- application/config/database.php
+++ application/config/database.php
@@ -65,7 +65,7 @@ $db['default'] = array(
     'dbcollat' => 'utf8_general_ci',
     'swap_pre' => '',
     'encrypt'  => array(
-		'ssl_ca' => 'DigiCertGlobalRootG2.crt.pem',
+		'ssl_ca' => 'config/certificates/DigiCertGlobalRootCA.crt.pem',
         'ssl_verify' => TRUE   // เปิดใช้งาน SSL/TLS
     ),
     'compress' => FALSE,
