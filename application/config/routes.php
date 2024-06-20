4d81d074681b691c6f23df478f66fc41aa9eff79
master <airbitprodev@gmail.com>
2024-06-20 13:26:17 +0700

    set base

--- application/config/routes.php
+++ application/config/routes.php
@@ -68,15 +68,7 @@ $route['pageNotFound'] = "user/pageNotFound";
 $route['checkEmailExists'] = "user/checkEmailExists";
 
 
-$route['addNewCondition']="cost/addNewCondition";
-$route['editCondition_cost/(:num)/(:num)'] = "cost/editCondition_cost/$1/$2";
-$route['editOldCondition']="cost/editOldCondition";
-$route['deleteCondition_cost']="cost/deleteCondition_cost";
-$route['addRegis_depart/(:num)/(:num)'] = "cost/addregis_depart/$1/$2";
-$route['addNewRegis_depart'] = "cost/addNewRegis_depart";
-$route['editRegis_depart/(:num)/(:num)'] = "cost/editRegis_depart/$1/$2";
-$route['editOldRegis_depart'] = "cost/editOldRegis_depart";
-$route['deleteRegis_depart']="cost/deleteRegis_depart";
+
 
 
 
