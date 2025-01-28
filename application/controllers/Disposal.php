3b1e02d88027a10f60ec11170ed5e941faf47b6d
master <airbitprodev@gmail.com>
2025-01-28 12:19:17 +0700

    test sum3

--- application/controllers/Disposal.php
+++ application/controllers/Disposal.php
@@ -21,15 +21,16 @@ class Disposal extends Admin_Controller
     }
 
 	public function testsum(){
+		$data["summary_dis1"]=[];
 		for ($i=0;$i<=12;$i++){
 			$summary=$this->disposal_model->sumTransport(3,$i+1);
 			if ($summary){
-				$data["summary_dis1"][$i] = $summary[0]->sum_dis;
-				echo ($data["summary_dis1"][$i]);
+				$data["summary_dis1"][] = $summary[0]->sum_dis;
+				//echo ($data["summary_dis1"][$i]);
 				echo "<br>";
 			}else{
-				$data["summary_dis1"][$i] =0;
-				echo ($data["summary_dis1"][$i]);
+				$data["summary_dis1"][] =0;
+			//	echo ($data["summary_dis1"][$i]);
 				echo "<br>";
 			}
 		}
