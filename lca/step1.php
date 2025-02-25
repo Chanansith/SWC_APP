997de8f3dbefe27764a9815df7e906534bad5fcf
master <airbitprodev@gmail.com>
2025-02-25 15:02:02 +0700

    add data

--- lca/step1.php
+++ lca/step1.php
@@ -95,13 +95,14 @@ $description = $_SESSION['description'] ?? 'No data';
             <th>Unit</th>
                 <th>CC</th>
                 <th>OD</th>
-                <th>TMF</th>
+                <th>pmf</th>
                 <th>TA</th>
                 <th>FE</th>
                 <th>ME</th>
                 <th>HT</th>
                 <th>TET</th>
                 <th>FET</th>
+                <th>MET</th>  
                 <th>MD</th>
                 <th>FD</th>
                 <th>POF</th>
@@ -114,13 +115,14 @@ $description = $_SESSION['description'] ?? 'No data';
               
                 <th>CC</th>
                 <th>OD</th>
-                <th>TMF</th>
+                <th>pmf</th>
                 <th>TA</th>
                 <th>FE</th>
                 <th>ME</th>
                 <th>HT</th>
                 <th>TET</th>
                 <th>FET</th>
+                <th>MET</th>
                 <th>MD</th>
                 <th>FD</th>
                 <th>POF</th>
@@ -226,7 +228,7 @@ $description = $_SESSION['description'] ?? 'No data';
         newCell6.innerHTML = "kg";
 
         const min_group1=6
-        const max_group1=23
+        const max_group1=24
         let col_no=1;
         for (let i = min_group1; i < max_group1; i++) {
             const newCell = newRow.insertCell(i);
@@ -246,90 +248,90 @@ $description = $_SESSION['description'] ?? 'No data';
                     }
                     if (col_no==3)
                     {
-                        cal_data=raw_item.ta;
-                        row_id=`ta_${currentrow}`;
+                        cal_data=raw_item.pmf;
+                        row_id=`pmf_${currentrow}`;
                     }
                     
                     if (col_no==4)
                     {
+                        cal_data=raw_item.ta;
+                        row_id=`ta_${currentrow}`;
+                    }
+                    if (col_no==5)
+                    {
                         cal_data=raw_item.fe;
                         row_id=`fe_${currentrow}`;
                     }
-                    if (col_no==5)
+                    if (col_no==6)
                     {
                         cal_data=raw_item.me;
                         row_id=`me_${currentrow}`;
                     }
-                    if (col_no==6)
-                    {
-                        cal_data=raw_item.ht;
-                        row_id=`me_${currentrow}`;
-                    }
                     if (col_no==7)
                     {
-                        cal_data=raw_item.tet;
-                        row_id=`me_${currentrow}`;
+                        cal_data=raw_item.ht;
+                        row_id=`ht_${currentrow}`;
                     }
                  
                     if (col_no==8)
                     {
                         cal_data=raw_item.tet;
-                        row_id=`me_${currentrow}`;
+                        row_id=`tet_${currentrow}`;
                     }
                  
                     if (col_no==9)
                     {
-                        cal_data=raw_item.tet;
-                        row_id=`me_${currentrow}`;
+                        cal_data=raw_item.fet;
+                        row_id=`fet_${currentrow}`;
                     }
                  
                     if (col_no==10)
                     {
                         cal_data=raw_item.met;
-                        row_id=`me_${currentrow}`;
+                        row_id=`met_${currentrow}`;
                     }
                  
                     if (col_no==11)
                     {
                         cal_data=raw_item.md;
-                        row_id=`me_${currentrow}`;
+                        row_id=`md_${currentrow}`;
                     }
                  
                     if (col_no==12)
                     {
                         cal_data=raw_item.fd;
-                        row_id=`me_${currentrow}`;
+                        row_id=`fd_${currentrow}`;
                     }
                  
                     if (col_no==13)
                     {
                         cal_data=raw_item.pof;
-                        row_id=`me_${currentrow}`;
+                        row_id=`pof_${currentrow}`;
                     }
                     if (col_no==14)
                     {
                         cal_data=raw_item.ir;
-                        row_id=`me_${currentrow}`;
+                        row_id=`ir_${currentrow}`;
                     }
                     if (col_no==15)
                     {
                         cal_data=raw_item.alo;
-                        row_id=`me_${currentrow}`;
+                        row_id=`alo_${currentrow}`;
                     }
                     if (col_no==16)
                     {
                         cal_data=raw_item.ulo;
-                        row_id=`me_${currentrow}`;
+                        row_id=`ulo_${currentrow}`;
                     } 
                     if (col_no==17)
                     {
                         cal_data=raw_item.nlo;
-                        row_id=`me_${currentrow}`;
+                        row_id=`nlo_${currentrow}`;
                     }
                     if (col_no==18)
                     {
                         cal_data=raw_item.wd;
-                        row_id=`me_${currentrow}`;
+                        row_id=`wd_${currentrow}`;
                     }
                  
                     
@@ -339,14 +341,14 @@ $description = $_SESSION['description'] ?? 'No data';
                     col_no++;
           
         }
-        const min_group2=23
-        const max_group2=40
+        const min_group2=24
+        const max_group2=42
 
         for (let i = min_group2; i < max_group2; i++) {
             const newCell = newRow.insertCell(i);
-         
+               var result_id="result_"+currentrow+"_"+i;
                 newCell.style.backgroundColor = 'green';
-                newCell.innerHTML = '<input type="text" value="0" readonly>';
+                newCell.innerHTML = `<input type="text" id="${result_id}" value="0" readonly>`;
           
           
         }
@@ -379,14 +381,143 @@ $description = $_SESSION['description'] ?? 'No data';
       
         // Get values from Amount (column 3) and CC (column 5)
         const amountInput =parseFloat(document.getElementById("amount_"+row_id).value);
-        const cc =parseFloat(document.getElementById("cc_"+row_id).value)
+        
+        const cc = parseFloat(document.getElementById("cc_" + row_id).value);
+       
+       
 
+//const pmf = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const ta = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const fe = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const me = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const ht = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const tet = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const fet = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const met = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const md = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+// const fd = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+// const pof = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+// const ir = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const alo = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+// const ulo = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const nlo = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+//const wd = 1; // เปลี่ยนค่าเริ่มต้นเป็น 1
+        const od = parseFloat(document.getElementById("od_" + row_id).value);
+        const pmf = parseFloat(document.getElementById("pmf_" + row_id).value);
+        const ta = parseFloat(document.getElementById("ta_" + row_id).value);
+        const fe = parseFloat(document.getElementById("fe_" + row_id).value);
+        const me = parseFloat(document.getElementById("me_" + row_id).value);
+        const ht = parseFloat(document.getElementById("ht_" + row_id).value);
+        const tet = parseFloat(document.getElementById("tet_" + row_id).value);
+        const fet = parseFloat(document.getElementById("fet_" + row_id).value);
+        const met = parseFloat(document.getElementById("met_" + row_id).value);
+        const md = parseFloat(document.getElementById("md_" + row_id).value);
+        const fd = parseFloat(document.getElementById("fd_" + row_id).value);
+         const pof = parseFloat(document.getElementById("pof_" + row_id).value);
+         const ir = parseFloat(document.getElementById("ir_" + row_id).value);
+         const alo = parseFloat(document.getElementById("alo_" + row_id).value);
+       const ulo = parseFloat(document.getElementById("ulo_" + row_id).value);
+      const nlo = parseFloat(document.getElementById("nlo_" + row_id).value);
+       const wd = parseFloat(document.getElementById("wd_" + row_id).value);
+
+        console.log(amountInput)
+        console.log(cc)
         // Calculate the product
-        const result = amount * cc;
+        const result_cc = amountInput * cc;
+        const result_od = amountInput * od;
+        const result_pmf = amountInput * pmf;
+        const result_ta = amountInput * ta;
+        const result_fe = amountInput * fe;
+        const result_me = amountInput * me;
+        const result_ht = amountInput * ht;
+        const result_tet = amountInput * tet;
+        const result_fet = amountInput * fet;
+        const result_met = amountInput * met;
+        const result_md = amountInput * md;
+        const result_fd = amountInput * fd;
+        const result_pof = amountInput * pof;
+        const result_ir = amountInput * ir;
+        const result_alo = amountInput * alo;
+        const result_ulo = amountInput * ulo;
+        const result_nlo = amountInput * nlo;
+        const result_wd = amountInput * wd;
+                
+        
 
+
+       console.log(result_cc)
         // Display the result in column 6 (or any other column)
-      //  const resultCell = row.cells[22]; // Column 6 (OD)
-       // resultCell.innerHTML = result.toFixed(2); // Show result with 2 decimal places
+ 
+
+       // CC
+    var result_id_cc = "result_" + row_id + "_24";
+    document.getElementById(result_id_cc).value = result_cc;
+
+    // OD
+    var result_id_od = "result_" + row_id + "_25";
+    document.getElementById(result_id_od).value = result_od;
+
+    // pmf
+    var result_id_pmf = "result_" + row_id + "_26";
+    document.getElementById(result_id_pmf).value = result_pmf;
+
+    // TA
+    var result_id_ta = "result_" + row_id + "_27";
+    document.getElementById(result_id_ta).value = result_ta;
+
+    // FE
+    var result_id_fe = "result_" + row_id + "_28";
+    document.getElementById(result_id_fe).value = result_fe;
+
+// ME
+    var result_id_me = "result_" + row_id + "_29";
+    document.getElementById(result_id_me).value = result_me;
+
+    // HT
+    var result_id_ht = "result_" + row_id + "_30";
+    document.getElementById(result_id_ht).value = result_ht;
+
+    // TET
+    var result_id_tet = "result_" + row_id + "_31";
+    document.getElementById(result_id_tet).value = result_tet;
+
+    // FET
+    var result_id_fet = "result_" + row_id + "_32";
+    document.getElementById(result_id_fet).value = result_fet;
+
+    var result_id_met = "result_" + row_id + "_33";
+    document.getElementById(result_id_met).value = result_met;
+    // MD
+    var result_id_md = "result_" + row_id + "_34";
+    document.getElementById(result_id_md).value = result_md;
+
+// FD
+    var result_id_fd = "result_" + row_id + "_35";
+    document.getElementById(result_id_fd).value = result_fd;
+
+    // POF
+    var result_id_pof = "result_" + row_id + "_36";
+    document.getElementById(result_id_pof).value = result_pof;
+
+    // IR
+    var result_id_ir = "result_" + row_id + "_37";
+    document.getElementById(result_id_ir).value = result_ir;
+
+    // ALO
+    var result_id_alo = "result_" + row_id + "_38";
+    document.getElementById(result_id_alo).value = result_alo;
+
+    // ULO
+    var result_id_ulo = "result_" + row_id + "_39";
+    document.getElementById(result_id_ulo).value = result_ulo;
+
+    // NLO
+    var result_id_nlo = "result_" + row_id + "_40";
+    document.getElementById(result_id_nlo).value = result_nlo;
+
+    // WD
+    var result_id_wd = "result_" + row_id + "_41";
+    document.getElementById(result_id_wd).value = result_wd;
 
        
     }
